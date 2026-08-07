c4bdfd157bd2e04c1888bce0d2978ccef13413c5
Ahmed Hesham <ah0115915v@gmail.com>
2026-08-07 12:48:32 +0300

    Theoretical Questions1

--- Assignment2_OOP/Program.cs
+++ Assignment2_OOP/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Theoretical Questions1
+
+
+            // a) Difference between a class and a struct:
+            // A class is a reference type, while a struct is a value type.
+            // Classes are stored by reference, while structs are stored by value.
+            // Classes can support 4pealers, but structs support only incapsolation.
+            // Classes are usually used for complex objects, while structs are suitable for small data types.
+
+            // b) Why are classes more suitable than structs for large applications?
+            // Classes are more suitable for large applications because they support inheritance,
+            // polymorphism, encapsulation, and reference sharing.
+            // They are better for representing complex objects and managing large amounts of data and behavior.
+
+            #endregion
+
+
         }
     }
 }
